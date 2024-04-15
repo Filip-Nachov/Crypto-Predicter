82599cf3cca8eeb2475379b1bd06270bc9fa5840
Filip <fnachov@gmail.com>
2024-04-15 18:10:31 +0200

    Made the data base

--- Sign/sign-up.py
+++ Sign/sign-up.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import sqlite3
 
-import database
+# connecting to a database 
+con = sqlite3.connect("database.db")
+# making a cursor object
+cur = con.cursor()
+ 
+# making the data base
+cur.execute("CREATE TABLE IF NOT EXISTS users(email TEXT, password TEXT)")
 
 app = Flask(__name__)
 
